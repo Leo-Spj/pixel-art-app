cd7ad9f3f56bfac20981769d3e17f63870bd44d3
Leonardo Espejo <85245534+Leo-Spj@users.noreply.github.com>
2024-09-01 12:37:12 -0500

    advertencia, dispositivo movil no soportado

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import EditorArtePixelado from './EditorArtePixelado';
 
 function App() {
+  useEffect(() => {
+    const esDispositivoMovil = () => {
+      return /Mobi|Android/i.test(navigator.userAgent);
+    };
+
+    const bloquearAccesoMovil = () => {
+      if (esDispositivoMovil()) {
+        alert('Esta página no es soportada en dispositivos móviles. Por favor, accede desde un navegador en un dispositivo de escritorio.');
+      }
+    };
+
+    bloquearAccesoMovil();
+  }, []);
+
   return (
     <div className="App">
       <EditorArtePixelado />
