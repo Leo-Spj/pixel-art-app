f4399866be437d933e1132a15406126475d298fe
Leonardo Espejo <85245534+Leo-Spj@users.noreply.github.com>
2024-09-09 11:23:24 -0500

    se recibe un archivo zip en lugar de un obj y mtl

--- src/components/opengl3D/ConversorObjOpenGl.js
+++ src/components/opengl3D/ConversorObjOpenGl.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
+import JSZip from 'jszip';
 import CargarArchivo from './CargarArchivo';
 import GenerarCodigo from './GenerarCodigo';
 import MostrarCodigo from './MostrarCodigo';
 import { parsearArchivoObj_Tinkercad, parsearArchivoMtl_Tinkercad, generarCodigoOpenGL_Tinkercad } from './CodigoTinkercad';
-import { parsearArchivoObj_FiguroIo, parsearArchivoMtl_FiguroIo, generarCodigoOpenGL_FiguroIo } from './CodigoFiguroio';
 import generarCodigoFiguroIo from './CodigoFiguroio';
 import './ConversorObjOpenGl.css';
 
@@ -13,11 +13,37 @@ const ConversorObjOpenGl = () => {
   const [codigo, setCodigo] = useState('');
   const [exitoCopia, setExitoCopia] = useState('');
 
-  const manejarCargaArchivo = (event, setArchivo) => {
+  const manejarCargaZip = (event) => {
     const archivo = event.target.files[0];
     const lector = new FileReader();
-    lector.onload = (e) => setArchivo(e.target.result);
-    lector.readAsText(archivo);
+
+    lector.onload = async (e) => {
+      const zip = await JSZip.loadAsync(e.target.result);
+      let objEncontrado = false;
+      let mtlEncontrado = false;
+
+      // Buscar el primer archivo .obj y .mtl dentro del .zip
+      zip.forEach((relativePath, zipEntry) => {
+        if (zipEntry.name.endsWith('.obj') && !objEncontrado) {
+          zipEntry.async('text').then((contenidoObj) => {
+            setArchivoObj(contenidoObj);
+          });
+          objEncontrado = true;
+        }
+        if (zipEntry.name.endsWith('.mtl') && !mtlEncontrado) {
+          zipEntry.async('text').then((contenidoMtl) => {
+            setArchivoMtl(contenidoMtl);
+          });
+          mtlEncontrado = true;
+        }
+      });
+
+      if (!objEncontrado || !mtlEncontrado) {
+        alert('No se encontraron archivos OBJ o MTL en el ZIP');
+      }
+    };
+
+    lector.readAsArrayBuffer(archivo);
   };
 
   const generarCodigoTinkercad = () => {
@@ -26,9 +52,6 @@ const ConversorObjOpenGl = () => {
     const codigo = generarCodigoOpenGL_Tinkercad(obj, materiales);
     setCodigo(codigo);
   };
-
-  
-
 
   const copiarAlPortapapeles = () => {
     navigator.clipboard.writeText(codigo).then(() => {
@@ -42,8 +65,7 @@ const ConversorObjOpenGl = () => {
   return (
     <div className="conversor-card">
       <h2>Convertidor OBJ a OpenGL</h2>
-      <CargarArchivo etiqueta="Subir OBJ:" aceptar=".obj" manejarCarga={(e) => manejarCargaArchivo(e, setArchivoObj)} />
-      <CargarArchivo etiqueta="Subir MTL:" aceptar=".mtl" manejarCarga={(e) => manejarCargaArchivo(e, setArchivoMtl)} />
+      <CargarArchivo etiqueta="Subir ZIP:" aceptar=".zip" manejarCarga={manejarCargaZip} />
       <GenerarCodigo generarCodigo={generarCodigoTinkercad} nombre="Tinkercad" />
       <GenerarCodigo
         generarCodigo={() => generarCodigoFiguroIo(setCodigo, archivoObj, archivoMtl)}
